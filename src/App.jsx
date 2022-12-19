e8159e0ba93b676ce2458882a752816304bc3396
utkarsh575 <utkarshlohani575@gmail.com>
2022-12-19 18:13:25 +0530

    :sparkles: added Navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import { useState , useEffect } from 'react'
-
+import React from "react";
+import { useState, useEffect } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./components/Navbar";
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [selectedPage, setSelectedPage] = useState("home");
+  const isAboveMediumScreen = useMediaQuery("(min-width:1060px");
 
-export default App  
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
+};
+
+export default App;
