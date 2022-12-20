2dbaaafb561e21fdcf37985917eedc027b2c8ddd
utkarsh575 <utkarshlohani575@gmail.com>
2022-12-20 09:54:58 +0530

    :lipstick: added DotGrup and Landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,8 +1,67 @@
 import useMediaQuery from "../hooks/useMediaQuery";
-const Landing = ({setSelectedPage}) => {
+import { motion } from "framer-motion";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+const Landing = ({ setSelectedPage }) => {
+  const isAboveLarge = useMediaQuery("(min-width:1060px)");
   return (
-    <div>Landing</div>
-  )
-}
+    <section
+      id="home"
+      className="md:flex md:justify-between md:items-center gap-16 md:h-full py-10"
+    >
+      <div className="basis-5/6 z-10 mt-16 md:mt-32 flex justify-between md:flex-row-reverse flex-col items-center ">
+        {isAboveLarge ? (
+          <div
+            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
+            before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
+          >
+            <img
+              alt="profile"
+              className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[15rem] md:max-w-[20rem]
+             "
+              src="./src/assets/profile.png"
+            />
+          </div>
+        ) : (
+          <div className="border-blue border-2 w-[20rem] rounded-t-full bg-gradient-to-b from-fuchsia-600 to-red scale-[85%]">
+            <img
+              alt="profile"
+              className=" z-10 w-full  max-w-[400px] md:max-w-[600px] "
+              src="./src/assets/profile.png"
+            />
+          </div>
+        )}
+        <div className="z-30 basis-2/5 mt-12 md:mt-32">
+          {/* HEADINGS */}
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <p className="text-6xl   font-playfair z-10 text-center md:text-start">
+              Utkarsh
+              <span
+                className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush
+                before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]"
+              >
+                575
+              </span>
+            </p>
+
+            <p className="mt-10 mb-7 text-sm text-center md:text-start">
+              Adipiscing arcu, in aliquam fringilla cursus. Elit arcu elementum
+              viverra malesuada sem ac faucibus dolor. Sagittis scelerisque.
+            </p>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Landing;
