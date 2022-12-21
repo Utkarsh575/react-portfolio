edc897efd2291799d1a4b477d8f7118d4543f668
utkarsh575 <utkarshlohani575@gmail.com>
2022-12-21 20:43:52 +0530

    :lipstick: styled lineBreak

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from "./components/Navbar";
 import DotGroup from "./components/DotGroup";
 import Landing from "./components/Landing";
+import LineBreak from "./components/LineBreak";
+import Skills from "./components/Skills";
 const App = () => {
   const [selectedPage, setSelectedPage] = useState("home");
   const isAboveMediumScreen = useMediaQuery("(min-width:1060px");
@@ -32,6 +34,10 @@ const App = () => {
           />
         )}
         <Landing setSelectedPage={setSelectedPage} />
+      </div>
+      <LineBreak />
+      <div className=" w-5/6 mx-auto md:h-full">
+        <Skills />
       </div>
     </div>
   );
---
+++ src/components/LineBreak.jsx
@@ -0,0 +1,5 @@
+const LineBreak = ({width ="w-full" }) => {
+  return <div className={`h-0.5 ${width} bg-gradient-rainblue`} />;
+}
+
+export default LineBreak
