5270d7900acef39c5fe990b7bd3a3f63e6f27501
utkarsh575 <utkarshlohani575@gmail.com>
2022-12-24 20:47:08 +0530

    :sparkles: added quotes to skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import LineBreak from "./LineBreak";
 
 const Skills = () => {
-  const isAboveSmallScreens = useMediaQuery("(min-width:768px)");
+  const isAboveSmallScreens = useMediaQuery("(min-width:1060px)");
   const [quote, setQuote] = useState("");
   useEffect(() => {
     fetch("https://api.adviceslip.com/advice")
@@ -20,23 +20,62 @@ const Skills = () => {
         My Skills
         <LineBreak width={"md:w-32 w-24"} />
       </div>
-      <h1 className="text-center bg-clip-text  bg-[url('../src/assets/brush.png') text-transparent text-7xl font-bold font-sans">
+      <h1 className="text-center bg-clip-text text-7xl font-bold font-sans">
         My Expertise{" "}
       </h1>
       {isAboveSmallScreens ? (
-        <div className="mt-10 items-center flex flex-row justify-start">
-          <div className="w-[33%] flex justify-center h-[20rem] text-center  items-start border-2 border-grey py-5 ]">
+        <div className="mt-10 items-center flex flex-row  justify-start  ">
+          <div className="basis-1/3 flex justify-center h-[20rem] text-center  items-center border-2 border-grey py-5 ]">
             <p>hi</p>
           </div>
-          <div className="w-[33%] flex justify-center h-[20rem] text-center  items-start border-2 border-grey py-5">
+          <div className="basis-1/3 flex justify-center h-[20rem] text-center  items-center border-2 border-grey py-5">
             <p>hi</p>
           </div>
-          <div className="w-[33%] flex justify-center h-[20rem] text-center  items-start border-2 border-grey py-5">
+          <div className="basis-1/3 flex justify-center h-[20rem] text-center  items-center border-2 border-grey py-5">
             <p>hi</p>
           </div>
         </div>
       ) : (
-        <div></div>
+        <div className="mt-10 items-center flex xs:flex-row flex-col-reverse flex-wrap w-auto justify-start  ">
+          <div className="xs:basis-1/2  flex justify-center h-[20rem] text-center  items-start border-2 border-grey py-5 ]">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque
+              repellendus laudantium quisquam et porro hic soluta neque impedit
+              ut aperiam!
+            </p>
+          </div>
+          <div className="xs:basis-1/2  flex justify-center h-[20rem] text-center  items-start border-2 border-grey py-5">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+              doloremque dolore nulla dicta modi rerum nobis ipsam debitis
+              corrupti aperiam?
+            </p>
+          </div>
+          <div className="xs:basis-1/2  flex justify-center h-[20rem] text-center  items-start border-2 border-grey py-5">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+              doloremque dolore nulla dicta modi rerum nobis ipsam debitis
+              corrupti aperiam?
+            </p>
+          </div>
+          <div className="xs:basis-1/2  flex justify-center h-[20rem] text-center  items-center border-2 border-grey py-5">
+            <blockquote className="text-xl italic font-semibold text-gray-900 dark:text-white">
+              <svg
+                aria-hidden="true"
+                className="w-10 h-10 text-gray-400 dark:text-gray-600"
+                viewBox="0 0 24 27"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+                  fill="currentColor"
+                />
+              </svg>
+              <p>"{quote}"</p>
+            </blockquote>
+          </div>
+        </div>
       )}
     </section>
   );
