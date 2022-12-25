8753529a8e3990a539e5ba3ce976372220c35107
utkarsh575 <utkarshlohani575@gmail.com>
2022-12-25 22:55:39 +0530

    :bento: fixed imports

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -25,7 +25,7 @@ module.exports = {
         opensans: ["Open Sans", "sans-serif"],
       },
       content: {
-        brush: "url('/public/brush.png')",
+        brush: "url('../public/brush.png')",
         person1: "url('./assets/person-1.png')",
         person2: "url('./assets/person-2.png')",
         person3: "url('./assets/person-3.png')",
